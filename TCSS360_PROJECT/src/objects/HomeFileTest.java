2ea96361c9e4892a1419eaac237a9b92a8b1fe19
Evan M <mcalliem@gmail.com>
2020-05-27 17:39:20 -0700

    File -> HomeFile, small edits, added HomeFileTest class, paths also updated to reflect changes.

---
+++ TCSS360_PROJECT/src/objects/HomeFileTest.java
@@ -0,0 +1,72 @@
+package objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.junit.jupiter.api.Test;
+
+class HomeFileTest 
+{
+	private HomeFile f; 
+	
+	@Test
+	public void testFileCreation()
+	{
+		f = new HomeFile("filename", "loren ipsum");
+		assertTrue(f != null);
+	}
+	
+	@Test
+	public void testGetName()
+	{
+		f = new HomeFile("filename", "loren ipsum");
+		assertEquals("filename", f.getFileName());
+	}
+	
+	@Test
+	public void testGetNotes()
+	{
+		f = new HomeFile("filename", "loren ipsum");
+		assertEquals("loren ipsum", f.getFileNotes());
+	}
+	
+	@Test
+	public void testGetDate()
+	{
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		String date = df.format(Calendar.getInstance().getTime());
+		f = new HomeFile("filename", "loren ipsum");
+		assertTrue(date.equals(f.getImportDate()));
+	}
+	
+	@Test
+	public void testAddNotes()
+	{
+		f = new HomeFile("filename", "loren ipsum");
+		f.addNote("a");
+		assertEquals("loren ipsum\na", f.getFileNotes());
+		f.addNote(" b ");
+		assertEquals("loren ipsum\na\n b ", f.getFileNotes());
+		f.addNote("");
+		assertEquals("loren ipsum\na\n b \n", f.getFileNotes());
+	}
+	
+	@Test
+	public void testClearNotes()
+	{
+		f = new HomeFile("filename", "loren ipsum");
+		f.clearNotes();
+		assertEquals("", f.getFileNotes());
+	}
+	
+	@Test
+	public void testChangeName()
+	{
+		f = new HomeFile("filename", "loren ipsum");
+		f.setNewFileName("newname");
+		assertEquals("newname", f.getFileName());
+	}
+}
